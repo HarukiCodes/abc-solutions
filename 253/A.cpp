68c44048f988c1596a0732475663794ca52b2ad9
HarukiCodes <haruking.86kun@gmail.com>
2025-10-01 02:51:31 +0900

    253_修正

--- 253/A.cpp
+++ 253/A.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    if ((a < b && b < c) || (a > b && b > c)) {
+    if ((a <= b && b <= c) || (a >= b && b >= c)) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
